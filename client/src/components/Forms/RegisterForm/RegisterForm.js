0178c0d8397f4f64fcd7d443dc62cc29a2d9e128
Aliel Reyes <alielreyes@gmail.com>
2020-10-12 12:00:05 -0700

    Register form values are being saved in the container. Needs to be validated.

--- client/src/components/Forms/RegisterForm/RegisterForm.js
+++ client/src/components/Forms/RegisterForm/RegisterForm.js
@@ -14,34 +14,34 @@ const RegisterForm = (props) => {
 
     return (
         <Form style={props.style} onSubmit={props.submit}>
-            <Form.Group controlId="formEmail">
+            <Form.Group controlId={props.formConfig.email.id}>
                 {props.disableLabels ? null: <Form.Label>Email address</Form.Label>}
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" onChange={(event) => props.change(event)} />
                 {formEmailText}
             </Form.Group>
 
-            <Form.Group controlId="formFirstName">
+            <Form.Group controlId={props.formConfig.fName.id}>
                 {props.disableLabels ? null : <Form.Label>First Name</Form.Label>}
-                <Form.Control type="text" placeholder="Enter First Name" />
+                <Form.Control type="text" placeholder="Enter First Name" onChange={(event) => props.change(event)}/>
             </Form.Group>
 
-            <Form.Group controlId="formLastName">
+            <Form.Group controlId={props.formConfig.lName.id}>
                 {props.disableLabels ? null : <Form.Label>Last Name</Form.Label>}
-                <Form.Control type="text" placeholder="Enter Last Name" />
+                <Form.Control type="text" placeholder="Enter Last Name" onChange={(event) => props.change(event)}/>
             </Form.Group>
 
-            <Form.Group controlId="formPassword">
+            <Form.Group controlId={props.formConfig.password.id}>
                 {props.disableLabels ? null: <Form.Label>Password</Form.Label>}
-                <Form.Control type="password" placeholder="Enter Password" />
+                <Form.Control type="password" placeholder="Enter Password" onChange={(event) => props.change(event)}/>
             </Form.Group>
 
-            <Form.Group controlId="formRetypePassword">
+            <Form.Group controlId={props.formConfig.retypePassword.id}>
                 {props.disableLabels ? null : <Form.Label>Retype Password</Form.Label>}
-                <Form.Control type="password" placeholder="Retype Password" />
+                <Form.Control type="password" placeholder="Retype Password" onChange={(event) => props.change(event)}/>
             </Form.Group>
 
-            <Form.Group controlId="formRegisterCheck">
-                <Form.Check type="checkbox" label="By checking you agree to our terms and policies" />
+            <Form.Group controlId={props.formConfig.registerCheck.id}>
+                <Form.Check type="checkbox" label="By checking you agree to our terms and policies" onChange={(event) => props.change(event)} />
             </Form.Group>
 
             <Button variant="primary" type="submit">
--- client/src/containers/Forms/RegisterFormContainer.js
+++ client/src/containers/Forms/RegisterFormContainer.js
@@ -1,48 +1,56 @@
-import React, { useReducer, useState } from 'react';
+import React, { useState } from 'react';
 import RegisterForm from '../../components/Forms/RegisterForm/RegisterForm';
 
+import { getFormElConfig, checkValidity } from '../../utils';
+
 const RegisterFormContainer = (props) => {
-    const initialState = {
-        email: null,
-        fName: null,
-        lName: null,
-        password: null,
-        retypePassword: null,
-        termsCheck: false
-    };
-
-    const reducer = (state, action) => {
-        switch(action.type) {
-            case 'email':
-                return { email: state.email }
-            case 'fName':
-                return { fName: state.fName }
-            case 'lName':
-                return { lName: state.lName }
-            case 'password':
-                return { password: state.password }
-            case 'retypePassword':
-                return { retypePassword: state.retypePassword }
-            case 'termsCheck':
-                return { termsCheck: state.termsCheck }
-            default:
-                throw new Error();
+    const [registerForm, setRegisterForm] = useState(
+        {
+            email: getFormElConfig('email'),
+            fName: getFormElConfig('firstName'),
+            lName: getFormElConfig('lastName'),
+            password: getFormElConfig('password'),
+            retypePassword: getFormElConfig('retypePassword'),
+            registerCheck: getFormElConfig('registerCheck')
         }
+    )
+
+    const handleChange = (event) => {
+
+        let form = { ...registerForm }
+        switch(event.target.id){
+            case (form.email.id):
+                form.email.value = event.target.value;
+                break;
+            case (form.fName.id):
+                form.fName.value = event.target.value;
+                break;
+            case (form.lName.id):
+                form.lName.value = event.target.value;
+                break;
+            case (form.password.id):
+                form.password.value = event.target.value;
+                break;
+            case (form.retypePassword.id):
+                form.retypePassword.value = event.target.value;
+                break;
+        }
+
+        setRegisterForm(form);
     }
-
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    // const [email, setEmail] = useState(null);
-    // const [fName, setFName] = useState(null);
-    // const [lName, setLName] = useState(null);
-    // const [password, setPassword] = useState(null);
-    // const [retypePassword, setRetypePassword] = useState(null);
-    // const [termsCheck, setTermsCheck] = useState(null);
+    /**
+     * Make sure the required values are not empty
+     * confirm that the email is an email
+     * first and last name need to be text
+     * password needs to be the following:
+     *  - minimum 8 characters long
+     *  - uses one special character
+     * retype password needs to match exactly to password.
+     */
 
     const handleSubmit = (event) => {
-        alert('A name was submitted: ' + state.email);
         event.preventDefault();
-
+        console.log(registerForm);
         // registerUser()
         // .then(res => {
         //     alert(res);
@@ -51,14 +59,16 @@ const RegisterFormContainer = (props) => {
         //     alert(error);
         // })
     }
+
     return (
         <RegisterForm 
             style={props.style}
             disableLabels={props.disableLabels }
             disableFormText={props.disableFormText}
             btnText={props.btnText}
+            formConfig={registerForm}
             submit={handleSubmit}
-            // change={(type) => dispatch({ type })} 
+            change={handleChange} 
             />
     );
 };
