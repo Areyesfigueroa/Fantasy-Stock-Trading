cd5a316f4607f6f2466ff32dc8d5099eac3132d6
Aliel Reyes <alielreyes@gmail.com>
2020-10-14 16:30:41 -0700

    Password is being hashed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,28 @@
 const db = require('../db');
+const bcrypt = require('bcrypt');
 
-exports.register = (request, response) => {
+exports.register = async (request, response) => {
     console.log(request.params);
-    // db.query('SELECT * FROM users', null, (err, res) => {
-    //     let dbData = [];
-    //     if(err) return next(err);
+    const data = request.params;
 
-    //     for (let row of res.rows) {
-    //         dbData.push(row);
-    //     }
-    //     response.send(dbData);
-    // });
+    try {
+        const saltRounds = 10;
+
+        const hashPassword = await bcrypt.hash(data.password, saltRounds);
+    
+        console.log(hashPassword);
+    
+        // db.query('SELECT * FROM users', null, (err, res) => {
+        //     let dbData = [];
+        //     if(err) return next(err);
+    
+        //     for (let row of res.rows) {
+        //         dbData.push(row);
+        //     }
+        //     response.send(dbData);
+        // });
+    } catch {
+
+    }
+
 };
