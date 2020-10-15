50cd603b9e9e5cee1069ae748e7abe8a99b51072
Aliel Reyes <alielreyes@gmail.com>
2020-10-14 20:04:51 -0700

    Added Insert Query

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,11 +7,25 @@ exports.register = async (request, response) => {
 
     try {
         const saltRounds = 10;
-
         const hashPassword = await bcrypt.hash(data.password, saltRounds);
-    
-        console.log(hashPassword);
-    
+        
+        const query = "INSERT INTO users(email, first_name, last_name, password, terms_and_policies_agreement) VALUES($1, $2, $3, $4, $5)";
+        const values = [data.email, data.fName, data.lName, hashPassword, data.terms_and_policies];
+
+        db.query(query, values, (err, res) => {
+            let dbData = [];
+            if(err) {
+                console.log(err); 
+                return;
+            }
+
+            for(let row of res.rows) {
+                dbData.push(row);    
+            }
+            
+            response.send(dbData);
+        });
+
         // db.query('SELECT * FROM users', null, (err, res) => {
         //     let dbData = [];
         //     if(err) return next(err);
