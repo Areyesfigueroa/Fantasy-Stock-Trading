be08a2cbce1ff65619927ec1c66970770e5e3b48
Aliel Reyes <alielreyes@gmail.com>
2020-11-15 13:23:51 -0800

    Testing

---
+++ __tests__/auth.test.js
@@ -0,0 +1,19 @@
+// const nock = require('nock');
+
+// const loginUser = require('../controllers/authController').login;
+
+// const fakeResponse = {
+//     user: {
+//         id: '1',
+//         email: 'aliel2@gmail.com',
+//         firstName: 'Aliel',
+//         lastName: 'Reyes'
+//     },
+//     sessionId: 'ThisisASessionID'
+// }
+
+// describe('Get user tests', () => {
+//     beforeEach(() => {
+//         nock('http://localhost:3000/api/auth/login')
+//     })
+// })
