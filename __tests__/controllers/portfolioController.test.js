5f37f093271baa534b08c6e94831aa0e944e15d6
Aliel Reyes <alielreyes@gmail.com>
2020-11-20 16:24:30 -0800

    Portfolio Controller unit tests completed

--- __tests__/controllers/portfolioController.test.js
+++ __tests__/controllers/portfolioController.test.js
@@ -1,32 +1,62 @@
-const portfolioDB = require('../db/portfolio');
-const authDB = require('../db/auth');
-const portfolioCtrl = require('../controllers/portfolioController');
-const StockErrorHandler = require('../error/StockErrorHandler');
+const authDB = require('../../db/auth');
+const portfolioCtrl = require('../../controllers/portfolioController');
+const authUserSessionService = require('../../services/auth/authUserSessionService');
+const getBalanceService = require('../../services/portfolio/getBalanceService');
 
-jest.mock('../db/auth');
-jest.mock('../db/portfolio');
+const StockErrorHandler = require('../../error/StockErrorHandler');
+
+jest.mock('../../db/auth');
+jest.mock('../../services/auth/authUserSessionService');
+jest.mock('../../services/portfolio/getBalanceService');
+
+const getMockedResponse = () => {
+    const response = { 
+        statusCode: 200,
+        send: jest.fn(obj => obj),
+        status: jest.fn(code => {statusCode=code; return response})
+    };
+
+    return response;
+}
 
 describe("Portfolio Controller Tests", () => {
     describe("Get User balance", () => {
-        //Test DB Query Params
-        //Test Response
-        //Test expired response
-        //Test no balance returned
-        //Test missing auth header
-        //Test error response
-        test('Response should return balance', async() => {
-            const request = { headers: { authorization: 'Bearer token' } };
-            const response = { send: jest.fn(obj => obj) };
-            const user = { user_id: 'userID' }
-            const balance = 100000;
 
-            authDB.hasUserSessionExpired.mockReturnValue(false);
-            authDB.getUserBySessionID.mockReturnValue(user);
-            portfolioDB.getAccountBalance.mockReturnValue(balance);
+        test('Response should send balance', async () => {
+            const request = { headers: { authorization: 'Bearer token' } };
+            const response = getMockedResponse();
+            const user = { user_id: 'userId' };
+            const balance = 100000;
+            const sessionId = 'sessionId';
+
+            await authUserSessionService.authUserSession.mockReturnValue(sessionId);
+            await authDB.getUserBySessionID.mockReturnValue(user);
+            await getBalanceService.getAccountBalance.mockReturnValue(balance);
 
             await portfolioCtrl.getBalance(request, response);
 
-            expect(response.send.mock.results[0].value).toEqual(balance);
+            expect(authUserSessionService.authUserSession).toHaveBeenCalledWith(request, response);
+            expect(authDB.getUserBySessionID).toHaveBeenCalledWith(sessionId);
+            expect(getBalanceService.getAccountBalance).toHaveBeenCalledWith(user.user_id);
+            expect(response.send).toHaveBeenCalledWith(balance);
+        });
+
+        test('Response should send error', async() => {
+            const request = {}
+            const response = getMockedResponse();
+
+            const errorMsg = "Mocked Error";
+            await authUserSessionService.authUserSession.mockImplementation(() => {
+                throw new Error(errorMsg);
+            });
+
+            await portfolioCtrl.getBalance(request, response);
+
+            //Assert
+            let error = response.send.mock.results[0].value;
+            let errorHandler = new StockErrorHandler(`Server Error, could not get account balance: ${errorMsg}`);
+            
+            expect(error).toEqual(errorHandler);
         })
     })
 })
