6e7824a1a50bdc7898177d43f13e122660581325
Aliel Reyes <alielreyes@gmail.com>
2020-12-03 17:03:08 -0800

    Removed sandbox instance from axios file.

--- axios.js
+++ axios.js
@@ -4,9 +4,4 @@ const instance = axios.create({
     baseURL: "https://cloud.iexapis.com/stable/"
 });
 
-const sandboxInstance = axios.create({
-    baseURL: "https://sandbox.iexapis.com/stable/"
-});
-
 exports.instance = instance;
-exports.sbInstance = sandboxInstance;
